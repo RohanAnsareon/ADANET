f70dddcaa4fe4e9d145f86aad8f53d470bab1c10
Orxan Ansar <ansar_o@itstep.edu.az>
2020-09-16 21:49:27 +0400

    Transaction

--- ADANET_FRAMEWORK/Program.cs
+++ ADANET_FRAMEWORK/Program.cs
@@ -7,35 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ADANET_FRAMEWORK
-{
-   class Program
-   {
-      static void Main(string[] args)
-      {
-         var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+namespace ADANET_FRAMEWORK {
+    class Program {
+        static void Main(string[] args) {
+            var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
 
-         var repo = new UserRepo(connectionString);
+            var repo = new UserRepo(connectionString);
 
-         repo.DeleteMultiple(new List<int> { 1, 3, 5 });
+            repo.DeleteMultipleWithTransaction(new List<int> { 22, 23, 24, -5 });
 
-         //var id = repo.Create(new User
-         //{
-         //   Name = "Nikolay",
-         //   Age = 24
-         //});
+            //var id = repo.Create(new User
+            //{
+            //   Name = "Nikolay",
+            //   Age = 24
+            //});
 
-         //Console.WriteLine(repo.Read(id));
+            //Console.WriteLine(repo.Read(id));
 
-         //repo.Update(id, new User
-         //{
-         //   Name = "John Doe",
-         //   Age = 25
-         //});
+            //repo.Update(id, new User
+            //{
+            //   Name = "John Doe",
+            //   Age = 25
+            //});
 
-         //Console.WriteLine(repo.Read(id));
+            //Console.WriteLine(repo.Read(id));
 
-         //repo.Delete(id);
-      }
-   }
+            //repo.Delete(id);
+        }
+    }
 }
--- ADANET_FRAMEWORK/Repositories/UserRepo.cs
+++ ADANET_FRAMEWORK/Repositories/UserRepo.cs
@@ -5,17 +5,17 @@ using System.Data.SqlClient;
 using System.Text;
 
 namespace ADANET_FRAMEWORK.Repos {
-   public class UserRepo {
-      private readonly string connString;
+    public class UserRepo {
+        private readonly string connString;
 
-      public UserRepo(string connString) {
-         this.connString = connString;
-      }
+        public UserRepo(string connString) {
+            this.connString = connString;
+        }
 
-      public int Create(User user) {
+        public int Create(User user) {
 
-         // SQL Expression
-         var sql = @"INSERT INTO [dbo].[User]
+            // SQL Expression
+            var sql = @"INSERT INTO [dbo].[User]
                                 ([Name]
                                 ,[Age])
                           VALUES
@@ -23,189 +23,197 @@ namespace ADANET_FRAMEWORK.Repos {
                                 ,@Age);
                      SET @id=SCOPE_IDENTITY()";
 
-         // Creating connection and opening
-         using (var connection = new SqlConnection(this.connString)) {
-            connection.Open();
+            // Creating connection and opening
+            using (var connection = new SqlConnection(this.connString)) {
+                connection.Open();
 
-            // command creating
-            using (var command = new SqlCommand(sql, connection)) {
+                // command creating
+                using (var command = new SqlCommand(sql, connection)) {
 
-               #region Adding parameters
-               #region InputParams
-               var nameParam = new SqlParameter("@Name", user.Name);
+                    #region Adding parameters
+                    #region InputParams
+                    var nameParam = new SqlParameter("@Name", user.Name);
 
-               command.Parameters.Add(nameParam);
+                    command.Parameters.Add(nameParam);
 
 
-               command.Parameters.AddWithValue("@Age", user.Age);
-               #endregion
+                    command.Parameters.AddWithValue("@Age", user.Age);
+                    #endregion
 
-               #region OutputParam
-               var idParam = new SqlParameter {
-                  ParameterName = "@id",
-                  SqlDbType = System.Data.SqlDbType.Int,
-                  Direction = System.Data.ParameterDirection.Output
-               };
+                    #region OutputParam
+                    var idParam = new SqlParameter {
+                        ParameterName = "@id",
+                        SqlDbType = System.Data.SqlDbType.Int,
+                        Direction = System.Data.ParameterDirection.Output
+                    };
 
-               command.Parameters.Add(idParam);
-               #endregion
+                    command.Parameters.Add(idParam);
+                    #endregion
 
-               #endregion
+                    #endregion
 
-               if (command.ExecuteNonQuery() == 0)
-                  throw new Exception("User was not inserted");
-               else
-                  return Convert.ToInt32(idParam.Value);
+                    if (command.ExecuteNonQuery() == 0)
+                        throw new Exception("User was not inserted");
+                    else
+                        return Convert.ToInt32(idParam.Value);
+                }
             }
-         }
-      }
+        }
 
-      public User Read(int id) {
-         var sql = @"SELECT * FROM [dbo].[User] WHERE Id = @Id";
+        public User Read(int id) {
+            var sql = @"SELECT * FROM [dbo].[User] WHERE Id = @Id";
 
-         using (var connection = new SqlConnection(this.connString)) {
-            connection.Open();
-            using (var command = new SqlCommand(sql, connection)) {
-               command.Parameters.AddWithValue("@Id", id);
+            using (var connection = new SqlConnection(this.connString)) {
+                connection.Open();
+                using (var command = new SqlCommand(sql, connection)) {
+                    command.Parameters.AddWithValue("@Id", id);
 
-               using (var reader = command.ExecuteReader()) {
-                  reader.Read();
+                    using (var reader = command.ExecuteReader()) {
+                        reader.Read();
 
-                  return new User {
-                     Id = Convert.ToInt32(reader["Id"]),
-                     Name = reader["Name"].ToString(),
-                     Age = Convert.ToInt32(reader["Age"])
-                  };
-               }
+                        return new User {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Name = reader["Name"].ToString(),
+                            Age = Convert.ToInt32(reader["Age"])
+                        };
+                    }
+                }
             }
-         }
-      }
+        }
 
-      public List<User> GetAll() {
-         var sql = @"SELECT * FROM [dbo].[User]";
+        public List<User> GetAll() {
+            var sql = @"SELECT * FROM [dbo].[User]";
 
-         using (var connection = new SqlConnection(this.connString)) {
-            connection.Open();
+            using (var connection = new SqlConnection(this.connString)) {
+                connection.Open();
 
-            var users = new List<User>();
+                var users = new List<User>();
 
-            using (var command = new SqlCommand(sql, connection)) {
-               using (var reader = command.ExecuteReader()) {
-                  while (reader.Read()) {
-                     users.Add(new User {
-                        Id = Convert.ToInt32(reader["Id"]),
-                        Name = reader["Name"].ToString(),
-                        Age = Convert.ToInt32(reader["Age"])
-                     });
-                  }
-               }
+                using (var command = new SqlCommand(sql, connection)) {
+                    using (var reader = command.ExecuteReader()) {
+                        while (reader.Read()) {
+                            users.Add(new User {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Name = reader["Name"].ToString(),
+                                Age = Convert.ToInt32(reader["Age"])
+                            });
+                        }
+                    }
+                }
+
+                return users;
             }
+        }
 
-            return users;
-         }
-      }
+        public List<User> GetByName(string name) {
+            var sql = @"SELECT * FROM [dbo].[User] WHERE Name = @Name";
 
-      public List<User> GetByName(string name) {
-         var sql = @"SELECT * FROM [dbo].[User] WHERE Name = @Name";
+            using (var connection = new SqlConnection(this.connString)) {
+                connection.Open();
 
-         using (var connection = new SqlConnection(this.connString)) {
-            connection.Open();
+                var users = new List<User>();
 
-            var users = new List<User>();
+                using (var command = new SqlCommand(sql, connection)) {
+                    command.Parameters.AddWithValue("@Name", name);
 
-            using (var command = new SqlCommand(sql, connection)) {
-               command.Parameters.AddWithValue("@Name", name);
+                    using (var reader = command.ExecuteReader()) {
+                        while (reader.Read()) {
+                            users.Add(new User {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Name = reader["Name"].ToString(),
+                                Age = Convert.ToInt32(reader["Age"])
+                            });
+                        }
+                    }
+                }
 
-               using (var reader = command.ExecuteReader()) {
-                  while (reader.Read()) {
-                     users.Add(new User {
-                        Id = Convert.ToInt32(reader["Id"]),
-                        Name = reader["Name"].ToString(),
-                        Age = Convert.ToInt32(reader["Age"])
-                     });
-                  }
-               }
+                return users;
             }
+        }
 
-            return users;
-         }
-      }
+        public void Update(int id, User user) {
+            var sql = @"UPDATE [dbo].[User] SET [Name] = @Name, [Age] = @Age WHERE Id = @Id";
 
-      public void Update(int id, User user) {
-         var sql = @"UPDATE [dbo].[User] SET [Name] = @Name, [Age] = @Age WHERE Id = @Id";
+            using (var connection = new SqlConnection(this.connString)) {
+                connection.Open();
 
-         using (var connection = new SqlConnection(this.connString)) {
-            connection.Open();
+                using (var command = new SqlCommand(sql, connection)) {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@Name", user.Name);
+                    command.Parameters.AddWithValue("@Age", user.Age);
 
-            using (var command = new SqlCommand(sql, connection)) {
-               command.Parameters.AddWithValue("@Id", id);
-               command.Parameters.AddWithValue("@Name", user.Name);
-               command.Parameters.AddWithValue("@Age", user.Age);
-
-               command.ExecuteNonQuery();
+                    command.ExecuteNonQuery();
+                }
             }
-         }
-      }
+        }
 
-      public void Delete(int id) {
-         var sql = @"DELETE FROM [dbo].[User]
+        public void Delete(int id) {
+            var sql = @"DELETE FROM [dbo].[User]
                      WHERE Id = @Id";
 
-         using (var connection = new SqlConnection(this.connString)) {
-            connection.Open();
-            using (var command = new SqlCommand(sql, connection)) {
-               command.Parameters.AddWithValue("@Id", id);
+            using (var connection = new SqlConnection(this.connString)) {
+                connection.Open();
+                using (var command = new SqlCommand(sql, connection)) {
+                    command.Parameters.AddWithValue("@Id", id);
 
-               if (command.ExecuteNonQuery() == 0)
-                  throw new Exception("User was not deleted");
+                    if (command.ExecuteNonQuery() == 0)
+                        throw new Exception("User was not deleted");
+                }
             }
-         }
-      }
+        }
 
-      public void DeleteMultiple(List<int> ids) {
-         //#region SqlExpression
-         //var sql = string.Empty;
+        public void DeleteMultiple(List<int> ids) {
+            var parameters = new string[ids.Count];
 
-         //sql += @"DELETE FROM [dbo].[User_1] WHERE Id in (";
-
-         //for (int i = 0; i < ids.Count; i++) {
-         //   sql += $"@Id{i}{(i < ids.Count - 1 ? "," : string.Empty)} ";
-         //}
-
-         //sql += ")";
-         //#endregion
-
-         //using (var connection = new SqlConnection(this.connString)) {
-         //   connection.Open();
-
-         //   using (var command = new SqlCommand(sql, connection)) {
-
-         //      for (int j = 0; j < ids.Count; j++) {
-         //         command.Parameters.AddWithValue($"@Id{j}", ids[j]);
-         //      }
-
-         //      if (command.ExecuteNonQuery() == 0) throw new Exception("There are no changes in user table");
-         //   }
-         //}
-
-         var parameters = new string[ids.Count];
-
-         for (int i = 0; i < ids.Count; i++) {
-            parameters[i] = string.Format("@Id{0}", i);
-         }
-
-         var sql = @"DELETE FROM [dbo].[User] WHERE Id IN(" + string.Join(", ", parameters) + ")";
-
-         using (var connection = new SqlConnection(this.connString)) {
-            connection.Open();
-            using (var command = new SqlCommand(sql, connection)) {
-               for (int i = 0; i < ids.Count; i++) {
-                  command.Parameters.AddWithValue(parameters[i], ids[i]);
-               }
-               if (command.ExecuteNonQuery() == 0)
-                  throw new Exception("User was not deleted");
+            for (int i = 0; i < ids.Count; i++) {
+                parameters[i] = string.Format("@Id{0}", i);
             }
-         }
-      }
-   }
+
+            var sql = @"DELETE FROM [dbo].[User] WHERE Id IN(" + string.Join(", ", parameters) + ")";
+
+            using (var connection = new SqlConnection(this.connString)) {
+                connection.Open();
+                using (var command = new SqlCommand(sql, connection)) {
+                    for (int i = 0; i < ids.Count; i++) {
+                        command.Parameters.AddWithValue(parameters[i], ids[i]);
+                    }
+                    if (command.ExecuteNonQuery() == 0)
+                        throw new Exception("User was not deleted");
+                }
+            }
+        }
+
+        public void DeleteMultipleWithTransaction(List<int> ids) {
+            var sql = @"DELETE FROM [User] WHERE Id = @Id";
+
+            using (var connection = new SqlConnection(this.connString)) {
+                connection.Open();
+                var transaction = connection.BeginTransaction("DeleteTransaction");
+                using (var command = connection.CreateCommand()) {
+                    command.Transaction = transaction;
+                    int i = 0;
+                    try {
+                        foreach (var id in ids) {
+                            i++;
+
+                            command.CommandText = $"{sql}{i}";
+                            command.Parameters.AddWithValue($"@Id{i}", id);
+
+                            command.ExecuteNonQuery();
+                        }
+                        transaction.Commit();
+
+                    } catch (Exception ex) {
+                        Console.WriteLine(ex.Message);
+
+                        try {
+                            transaction.Rollback();
+                        } catch (Exception ex2) {
+                            Console.WriteLine(ex2.Message);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
